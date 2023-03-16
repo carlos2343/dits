70ec24a61989388bdc50f22b82117c1703af8db2
IanBurley <ianburley2019@gmail.com>
2023-03-16 10:56:48 -0500

    add components

--- FH/src/app/app-routing.module.ts
+++ FH/src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { LandingComponent } from './components/landing/landing.component';
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [{
@@ -9,11 +11,19 @@ const routes: Routes = [{
   {
   path: 'login', 
   component: LoginComponent},
+  {
+  path: 'landing',
+  component: LandingComponent},
+  {
+  path: 'sign-up',
+  component: LandingComponent},
   ];
 
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
