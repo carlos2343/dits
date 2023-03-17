bd444403b6b23a173f67523772a7844ea6be35f9
IanBurley <ianburley2019@gmail.com>
2023-03-16 19:58:09 -0500

    page works

--- FH/src/app/components/login/login.component.ts
+++ FH/src/app/components/login/login.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required)
+  });
 
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  submit() {
+    // do something with the form data, like sending it to a server
+    console.log(this.loginForm.value);
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+  
 }
